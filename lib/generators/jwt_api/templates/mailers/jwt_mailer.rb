306796e387cc72d51ffe1089b926124aa193de60
Leo Policastro <lpolicastro@pm.me>
2021-08-27 09:50:32 -0400

    password reset mailer

--- lib/generators/jwt_api/templates/mailers/jwt_mailer.rb
+++ lib/generators/jwt_api/templates/mailers/jwt_mailer.rb
@@ -2,7 +2,7 @@ class JwtMailer < ApplicationMailer
   def reset_password(user_id, raw_token)
     @resource = User.find(user_id)
     # TODO: add url environement variable
-    @token_link = "http://localhost:3000/users/password/edit?token=#{raw_token}"
+    @token_link = "http://localhost:3000/api/users/verify_password_reset?token=#{raw_token}"
     @token = raw_token
     mail(to: @resource.email, subject: 'Reset password instructions',
          template_name: 'reset_password')
